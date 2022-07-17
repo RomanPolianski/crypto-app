c9a8980e0420a3d3c174cda655ef6a83973bb863
Roman <rpol123453@gmail.com>
2022-07-17 23:29:21 +0300

    Add modal to buy coin

---
+++ src/components/Modals/BuyCurrencyModal/BuyCurrencyModal.tsx
@@ -0,0 +1,75 @@
+import React, { FC, useState } from 'react';
+import ReactDOM from 'react-dom';
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../../../store/cartSlice';
+import s from './BuyCurrencyModal.module.scss';
+
+interface BuyCurrencyModalProps {
+  open: boolean;
+  close: Function;
+  name: string;
+}
+
+const modalRootElement = document.querySelector('#modal') as HTMLDivElement;
+
+const BuyCurrencyModal: FC<BuyCurrencyModalProps> = ({
+  open,
+  close,
+  name,
+}): JSX.Element => {
+  const [amount, setAmount] = useState<string>('0');
+
+  const dispatch = useDispatch();
+  const handleAddToCart = () => {
+    dispatch(addToCart({ name, amount }));
+  };
+
+  return ReactDOM.createPortal(
+    <div>
+      {open && (
+        <div className={s.modalContainer}>
+          <div className={s.modal}>
+            <h1>Buy {name} coin</h1>
+            <p>Enter the amount</p>
+            <input
+              className={s.inputAmount}
+              type="number"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setAmount(e.target.value)
+              }
+              value={amount}
+              step={0.01}
+            />
+            <button
+              className={s.modalSubmit}
+              type="button"
+              onClick={handleAddToCart}
+            >
+              Buy
+            </button>
+            <button
+              type="button"
+              className={s.closeModalIcon}
+              onClick={() => close()}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                fill="currentColor"
+                className="bi bi-x"
+                viewBox="0 0 16 16"
+              >
+                {' '}
+                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />{' '}
+              </svg>
+            </button>
+          </div>
+        </div>
+      )}
+    </div>,
+    modalRootElement
+  );
+};
+
+export default BuyCurrencyModal;
