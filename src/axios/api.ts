d4bf49b31d94398c226190b9b1c51162b6da8920
Roman <rpol123453@gmail.com>
2022-07-22 15:18:54 +0300

    Add .env config file

--- src/axios/api.ts
+++ src/axios/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = 'https://api.coincap.io/v2/';
+export const API_URL = import.meta.env.VITE_API_URL;
 
 const instance = axios.create({
   baseURL: API_URL,
