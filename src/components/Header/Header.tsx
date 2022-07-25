83280364816afd9c8df82346e4c8c2e88c76b90e
Roman <rpol123453@gmail.com>
2022-07-26 01:08:36 +0300

    Fix cart status info in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,19 +12,17 @@ const Header: FC = (): JSX.Element => {
   const topCoins = useSelector((state: RootState) =>
     state.currency.currencies.slice(0, 3)
   );
-
   const totalCartItemsAmount = useSelector(
     (state: RootState) => state.cart.cartTotalQuantity
   );
-
-  const totalCartNow = useSelector(
-    (state: RootState) => state.cart.cartTotalNow
+  const totalCartDifference = useSelector(
+    (state: RootState) => state.cart.prevDifferenceCartTotal
   );
   const totalCartDifferencePercent = useSelector(
-    (state: RootState) => state.cart.differenceCartTotalPercent
+    (state: RootState) => state.cart.prevDifferenceCartTotalPercent
   );
-  const totalCartDifference = useSelector(
-    (state: RootState) => state.cart.differenceCartTotal
+  const cartTotalNow = useSelector(
+    (state: RootState) => state.cart.histCartTotal[0]
   );
 
   const navigate = useNavigate();
@@ -32,32 +30,52 @@ const Header: FC = (): JSX.Element => {
   return (
     <header className={s.header}>
       <div className={s.headerInnerContainer}>
-        <img src={Logo} alt="logo" className={s.logo} />
-        <h1 className={s.appName}>CRYPTO</h1>
-        {topCoins.map((c) => {
-          return (
-            <div key={c.name} className={s.topCoinWrapper}>
-              <div className={s.topCoinContainer}>
-                <p className={s.textCoin}>{c.name}</p>
-                <p className={s.textCoin}>{toUSD.format(Number(c.priceUsd))}</p>
+        <div className={s.headerLeft}>
+          <img src={Logo} alt="logo" className={s.logo} />
+          <h1 className={s.appName}>CRYPTO</h1>
+          {topCoins.map((c) => {
+            return (
+              <div key={c.name} className={s.topCoinWrapper}>
+                <div className={s.topCoinContainer}>
+                  <p className={s.textCoin}>{c.name}</p>
+                  <p className={s.textCoin}>
+                    {toUSD.format(Number(c.priceUsd))}
+                  </p>
+                </div>
+                <i
+                  className={classNames(
+                    s.arrow,
+                    Number(c.changePercent24Hr) < 0 ? s.down : s.up
+                  )}
+                />
               </div>
-              <i
-                className={classNames(
-                  s.arrow,
-                  Number(c.changePercent24Hr) < 0 ? s.down : s.up
-                )}
-              />
-            </div>
-          );
-        })}
-        <button
-          type="button"
-          className={s.cartButton}
-          onClick={() => navigate('/cart')}
-        >
-          <PortfolioSvg />
-        </button>
-        <p className={s.coinNumberInCart}>{totalCartItemsAmount}</p>
+            );
+          })}
+        </div>
+
+        <div className={s.cart}>
+          <p className={s.cartStatusWrapper}>
+            <span>{toUSD.format(cartTotalNow)}</span>
+            <span className={s.cartStatusText}>
+              USD {totalCartDifference.toFixed(2)}{' '}
+              <span>
+                {cartTotalNow
+                  ? `(${totalCartDifferencePercent.toFixed(2)}%)`
+                  : ''}
+              </span>
+            </span>
+          </p>
+          <span className={s.cartIconWrapper}>
+            <button
+              type="button"
+              className={s.cartButton}
+              onClick={() => navigate('/cart')}
+            >
+              <PortfolioSvg />
+            </button>
+            <p className={s.coinNumberInCart}>{totalCartItemsAmount}</p>
+          </span>
+        </div>
       </div>
     </header>
   );
