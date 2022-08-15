612b4e9f1d40c36881a5519bf97b8e9cf232768e
Roman <rpol123453@gmail.com>
2022-08-15 19:51:01 +0300

    Create unit test for usdFormatter util

---
+++ src/utils/formatters/toUSDFormatter.test.ts
@@ -0,0 +1,22 @@
+import { toUSD } from './toUSDformatter';
+
+describe('toUSD test', () => {
+  it('returns correct val', () => {
+    expect(toUSD(12)).toEqual('US$12.00');
+  });
+  it('returns val', () => {
+    expect(toUSD(0.12)).toEqual('US$0.12');
+  });
+  it('returns val', () => {
+    expect(toUSD(0.001)).toEqual('US$0.001000');
+  });
+  it('returns val', () => {
+    expect(toUSD(0.000011)).toEqual('US$0.000011');
+  });
+  it('returns val', () => {
+    expect(toUSD(-0.000011)).toEqual('error');
+  });
+  it('returns val', () => {
+    expect(toUSD(-0.1)).toEqual('error');
+  });
+});
