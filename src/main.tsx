f8acd22acb89e9b802cfbdd687a3ba54f05a0b80
Roman <rpol123453@gmail.com>
2022-08-18 20:14:24 +0300

    Install & configure apollo

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { ApolloProvider } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
@@ -5,12 +6,15 @@ import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 import './index.scss';
 import { persistor, store } from './store/index';
+import client from './apollo/client';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <App />
+        <ApolloProvider client={client}>
+          <App />
+        </ApolloProvider>
       </PersistGate>
     </Provider>
   </React.StrictMode>
